27f946433b96204376c177b047fac55d2be1610b
Jonatan Troche <trochealmediajonatan@gmail.com>
2026-03-06 02:16:19 -0400

    home page assignment

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,4 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").innerHTML = currentYear;
+
+document.getElementById("lastmodified").innerHTML = "lastmodified: " + document.lastModified;
